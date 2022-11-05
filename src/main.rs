a2af5c257ea92cd4df02411ff174f21921583c5f
Jay T <jaytula@gmail.com>
2022-11-05 09:14:22 -0700

    Video progress 18:30

--- src/main.rs
+++ src/main.rs
@@ -27,4 +27,12 @@ fn main_lesson_02() {
 }
 
 fn main() {
+  // Unsigned integer: u8, u16, u32, u64, u128, u256, usize
+  // Signed integer: i8, i16, i32, i64, i128, isize
+  println!("Max u32 : {}", u32::MAX);
+  println!("Max u64 : {}", u64::MAX);
+  println!("Max usize : {}", usize::MAX);
+  println!("Max u128 : {}", u128::MAX);
+  println!("Max f32 : {}", f32::MAX);
+  println!("Max f64 : {}", f64::MAX);
 }
