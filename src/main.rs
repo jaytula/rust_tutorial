8ce98f61edd7218a5f6acac72d78358290435759
Jay T <jaytula@gmail.com>
2022-11-05 09:38:13 -0700

    Video Progress 26:26

--- src/main.rs
+++ src/main.rs
@@ -66,4 +66,14 @@ fn main_lesson_07() {
 }
 
 fn main() {
+  let age: i32 = 8;
+  if (age >= 1) && (age <= 18) {
+    println!("Important Birthday");
+  } else if (age == 21) || (age == 50) {
+    println!("Important Birthday");
+  } else if age >= 65 {
+    println!("Important Birthday");
+  } else {
+    println!("Not an Important Birthday");
+  }
 }
