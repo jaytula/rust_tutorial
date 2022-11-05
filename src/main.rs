f8d41bdb5eee9446e63812a58b5d4b93e08ae35e
Jay T <jaytula@gmail.com>
2022-11-05 09:44:40 -0700

    Video Progress 30:18

--- src/main.rs
+++ src/main.rs
@@ -89,4 +89,11 @@ fn main_lesson_09() {
 }
 
 fn main() {
+  let age2 = 8;
+  match age2 {
+    1..=18 => println!("Important Birthday"),
+    21 | 50 => println!("Important Birthday"),
+    65..=i32::MAX => println!("Important Birthday"),
+    _ => println!("Not an Important Birthday"),
+  };
 }
