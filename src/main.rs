138ed70c9677c2a5cd6124f235e897adec155f35
Jay T <jaytula@gmail.com>
2022-11-05 12:19:43 -0700

    Video Progress 1:11:29

--- src/main.rs
+++ src/main.rs
@@ -302,6 +302,13 @@ fn main_lesson_26() {
   println!("Sum of list = {}", sum_list(&num_list));
 }
 
+use std::ops::Add;
+
+fn get_sum_gen<T:Add<Output = T>>(x: T, y: T) -> T {
+  return x + y;
+}
+
 fn main() {
-  
+  println!("5 + 4 = {}", get_sum_gen(5, 4))
+  println!("5.2 + 4.6 = {}", get_sum_gen(5.2, 4.6))
 }
