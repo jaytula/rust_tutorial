dc168bbbebe00ff33ae3226ce1982845459432d5
Jay T <jaytula@gmail.com>
2022-11-05 09:03:05 -0700

    Rename to main_lesson_01

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
-fn main() {
+fn main_lesson_01() {
     println!("What is your name?");
     let mut name: String = String::new();
     let greeting: &str = "Nice to meet you";
     io::stdin().read_line(&mut name)
       .expect("Didn't Receive Input");
     println!("Hello, {}! {}", name.trim_end(), greeting);
+}
+
+fn main() {
+
 }
