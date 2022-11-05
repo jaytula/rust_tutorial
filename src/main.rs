a8fc7468e6d750bc153abeb496747a40d5e9c333
Jay T <jaytula@gmail.com>
2022-11-05 09:56:42 -0700

    Video Progress 37:18

--- src/main.rs
+++ src/main.rs
@@ -109,4 +109,20 @@ fn main_lesson_11() {
 }
 
 fn main() {
+  let arr_2 = [1,2,3,4,5,6,7,8,9];
+  println!("1st : {}", arr_2[0]);
+  println!("Length: {}", arr_2.len());
+  
+  let mut loop_idx = 0;
+  loop {
+    if arr_2[loop_idx] % 2 == 0 {
+      loop_idx += 1;
+      continue;
+    }
+    if arr_2[loop_idx] == 9 {
+      break;
+    }
+    println!("Val : {}", arr_2[loop_idx]);
+    loop_idx += 1;
+  }
 }
