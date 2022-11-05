47dc47901f598f8dae9ab015ea122a55d05b7773
Jay T <jaytula@gmail.com>
2022-11-05 11:41:50 -0700

    Rename to main_lesson_18

--- src/main.rs
+++ src/main.rs
@@ -187,9 +187,12 @@ fn main_lesson_17() {
   }
 }
 
-fn main() {
+fn main_lesson_18() {
   let int_u8: u8 = 5;
   let int2_u8: u8 = 4;
   let int3_u32: u32 = int_u8 as u32 + int2_u8 as u32;
   println!("{}", int3_u32);
+}
+
+fn main() {
 }
