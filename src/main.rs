294cae952123bcd945555dff66ab10f41e467f29
Jay T <jaytula@gmail.com>
2022-11-05 09:40:40 -0700

    Video Progress 27:50

--- src/main.rs
+++ src/main.rs
@@ -79,4 +79,11 @@ fn main_lesson_08() {
 }
 
 fn main() {
+  let mut my_age = 47;
+  let can_vote = if my_age >= 18 {
+    true
+  } else {
+    false
+  };
+  println!("Can Vote : {}", can_vote);
 }
