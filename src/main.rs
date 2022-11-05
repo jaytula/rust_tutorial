a91bb64740f703411069c2becf3542ea4248d34f
Jay T <jaytula@gmail.com>
2022-11-05 10:06:20 -0700

    Rename to main_lesson_15

--- src/main.rs
+++ src/main.rs
@@ -144,9 +144,12 @@ fn main_lesson_14() {
   }
 }
 
-fn main() {
+fn main_lesson_15() {
   let my_tuple: (u8, String, f64) = (47, "Derek".to_string(), 50_000.00);
   println!("Name : {}", my_tuple.1);
   let (v1, v2, v3) = my_tuple;
   println!("Age : {}", v1);
+}
+
+fn main() {
 }
