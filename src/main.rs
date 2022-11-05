2b4960d9345f03e47a365a5e15049f4f5134d3e3
Jay T <jaytula@gmail.com>
2022-11-05 10:05:59 -0700

    Video Progress 41:26

--- src/main.rs
+++ src/main.rs
@@ -145,4 +145,8 @@ fn main_lesson_14() {
 }
 
 fn main() {
+  let my_tuple: (u8, String, f64) = (47, "Derek".to_string(), 50_000.00);
+  println!("Name : {}", my_tuple.1);
+  let (v1, v2, v3) = my_tuple;
+  println!("Age : {}", v1);
 }
