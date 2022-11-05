d312d1b1d9222ce69c7c4f4529afafb99440b525
Jay T <jaytula@gmail.com>
2022-11-05 10:11:42 -0700

    Video Progress 44:10

--- src/main.rs
+++ src/main.rs
@@ -152,4 +152,13 @@ fn main_lesson_15() {
 }
 
 fn main() {
+  let mut st1 = String::new();
+  st1.push('A');
+  st1.push_str(" word");
+  for word in st1.split_whitespace() {
+    println!("{}", word);
+  }
+
+  let st2 = st1.replace("A", "Another");
+  println!("{}", st2);  
 }
