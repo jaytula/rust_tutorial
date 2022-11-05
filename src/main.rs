fb9f61111ed28dcf3047c430c41d128567feb92a
Jay T <jaytula@gmail.com>
2022-11-05 11:50:42 -0700

    Video Progress 55:56

--- src/main.rs
+++ src/main.rs
@@ -195,4 +195,35 @@ fn main_lesson_18() {
 }
 
 fn main() {
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+
+  impl Day {
+    fn is_weekend(&self) -> bool {
+      match self {
+        Day::Saturday | Day::Sunday => true,
+        _ => false
+      }
+    }
+  }
+
+  let today = Day::Monday;
+  match today {
+    Day::Monday => println!("Everyone hates Monday"),
+    Day::Tuesday => println!("Donut day"),
+    Day::Wednesday => println!("Hump day"),
+    Day::Thursday => println!("Pay day"),
+    Day::Friday => println!("Almost weekend"),
+    Day::Saturday => println!("weekend"),
+    Day::Sunday => println!("weekend"),
+  }
+
+  println!("Is today the weekend {}", today.is_weekend());
 }
