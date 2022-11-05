90c96c85655777ab52993252da6c4b1c1d68ecdb
Jay T <jaytula@gmail.com>
2022-11-05 12:15:10 -0700

    Video Progress 1:07:43

--- src/main.rs
+++ src/main.rs
@@ -289,5 +289,15 @@ fn main_lesson_25() {
   println!("Nums : {} {}", val_1, val_2);
 }
 
+fn sum_list(list: &[i32]) -> i32 {
+  let mut sum = 0;
+  for &val in list.iter() {
+    sum += &val;
+  }
+  sum
+}
+
 fn main() {
+  let num_list = vec![1,2,3,4,5];
+  println!("Sum of list = {}", sum_list(&num_list));
 }
