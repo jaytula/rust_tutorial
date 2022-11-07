49f81debf0f3e0899e5a2e72798632375d4ef825
Jay T <jaytula@gmail.com>
2022-11-06 20:03:25 -0800

    Video Progress 1:24:47

--- src/main.rs
+++ src/main.rs
@@ -345,5 +345,24 @@ fn main_lesson_28() {
   println!("str is {}", str1);
 }
 
+use std::collections::HashMap;
+
 fn main() {
+  let mut heroes = HashMap::new();
+  heroes.insert("Superman", "Clark Kent");
+  heroes.insert("Batman", "Bruce Wayne");
+  heroes.insert("Flash", "Barry Allen");
+
+  for(k, v) in heroes.iter() {
+    println!("{} = {}", k, v);
+  }
+  println!("Length : {}", heroes.len());
+
+  if heroes.contains_key(&"Batman") {
+    let the_batman = heroes.get(&"Batman");
+    match the_batman {
+      Some(x) => println!("Batman is a hero"),
+      None => println!("Batman is not a hero"),
+    }
+  }
 }
