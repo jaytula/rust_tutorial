028c47f3d356bbff08d186e6cff7b851f73da7a5
Jay T <jaytula@gmail.com>
2022-11-07 22:25:57 -0800

    Video Progress 1:34:18

--- src/main.rs
+++ src/main.rs
@@ -384,13 +384,35 @@ fn main_lesson_30() {
 }
 
 fn main() {
-  struct Rectangle<T, U> {
-    length: T,
-    height: U,
-  }
-  let rec = Rectangle{length: 4, height: 10.5};
+  const PI: f32 = 3.14159;
   trait Shape {
     fn new(length: f32, width: f32) -> Self;
     fn area(&self) -> f32;
   }
+  struct Rectangle {length: f32, width: f32};
+  struct Circle {length: f32, width: f32};
+
+  impl Shape for Rectangle {
+    fn new(length: f32, width: f32) -> Rectangle {
+      return Rectangle { length, width };
+    }
+    fn area(&self) -> f32 {
+      return self.length * self.width;
+    }
+  }
+
+  impl Shape for Circle {
+    fn new(length: f32, width: f32) -> Circle {
+      return Circle { length, width };
+    }
+    fn area(&self) -> f32 {
+      return (self.length / 2.0).powf(2.0) * PI;
+    }
+  }
+
+  let rec: Rectangle = Shape::new(10.0, 10.0);
+  let circ: Circle = Shape::new(10.0, 10.0);
+
+  println!("Rec Area : {}", rec.area());
+  println!("Circle Area : {}", circ.area());
 }
