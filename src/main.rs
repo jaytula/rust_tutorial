40cd125131ff6f01d2f87cb218ab808c0df9e1d4
Jay T <jaytula@gmail.com>
2022-11-10 12:18:40 -0800

    Video Progress 1:55:46

--- src/main.rs
+++ src/main.rs
@@ -459,4 +459,22 @@ fn main() {
   };
   write!(output, "Just some\nRandom words").expect("Failed to write to file");
   
+  let input = File::open(path).unwrap();
+  let buffered = BufReader::new(input);
+  for line in buffered.lines()
+  {
+    println!("{}", line.unwrap());
+  }
+
+  let output2 = File::create("rand.txt");
+  let output2 = match output2 {
+    Ok(file) => file,
+    Err(error) => match error.kind() {
+      ErrorKind::NotFound => match File::create("rand.txt") {
+        Ok(fc) => fc,
+        Err(e) => panic!("Can't create file {:?}", error),
+      },
+      _other_error => panic!("problem opening file : {:?}", error),
+    },
+  };
 }
