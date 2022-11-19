fad0a4aba93dcef8afd2722db513b208b8af0638
Jay T <jaytula@gmail.com>
2022-11-18 17:24:01 -0800

    Video Progress 2:05:50

--- src/main.rs
+++ src/main.rs
@@ -513,4 +513,11 @@ fn main_lesson_38() {
 }
 
 fn main() {
+  fn use_func<T>(a: i32, b: i32, func: T) -> i32 where T: Fn(i32, i32) -> i32 {
+    func(a, b)
+  }
+  let sum = |a, b| a+b;
+  let prod = |a, b| a*b;
+  println!("5 + 4 = {}", use_func(5, 4, sum));
+  println!("5 * 4 = {}", use_func(5, 4, prod));
 }
