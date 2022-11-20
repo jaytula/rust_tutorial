0bbde2da93115cd70206ad90384816668d464752
Jay T <jaytula@gmail.com>
2022-11-20 10:14:41 -0800

    Video Progress 2:15:06

--- src/main.rs
+++ src/main.rs
@@ -533,4 +533,27 @@ fn main_lesson_40() {
 }
 
 fn main() {
+  struct TreeNode<T> {
+    pub left: Option<Box<TreeNode<T>>>,
+    pub right: Option<Box<TreeNode<T>>>,
+    pub key: T,
+  }
+
+  impl<T> TreeNode<T> {
+    pub fn new(key: T) -> Self {
+      TreeNode { left: None, right: None, key }
+    }
+    pub fn left(mut self, node: TreeNode<T>) -> Self {
+      self.left = Some(Box::new(node));
+      self
+    }
+    pub fn right(mut self, node: TreeNode<T>) -> Self {
+      self.right = Some(Box::new(node));
+      self
+    }
+  }
+
+  let node1 = TreeNode::new(1)
+  .left(TreeNode::new(2))
+  .right(TreeNode::new(3));
 }
