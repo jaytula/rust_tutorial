0ad590ad93e86f64b1b072a249d5a7e0b4b6fc61
Jay T <jaytula@gmail.com>
2022-11-21 11:00:13 -0800

    Video Progress 2:19:50

--- src/main.rs
+++ src/main.rs
@@ -558,9 +558,24 @@ fn main_lesson_41() {
   .right(TreeNode::new(3));
 }
 
+use std::thread;
+use std::time::Duration;
+
 fn main() {
   // Common problems with parallel programming involve:
   // 1. Thread are accessing data in the wrong order
   // 2. Threads are blocked from executing because of confusion
   // over requirements to proceed with execution
+
+  thread::spawn(|| {
+    for i in 1..25 {
+      println!("Spawned thread : {}", i);
+      thread::sleep(Duration::from_millis(1));
+    }
+  });
+
+  for i in 1..20 {
+    println!("Main thread : {}", i);
+    thread::sleep(Duration::from_millis(1));
+  }
 }
