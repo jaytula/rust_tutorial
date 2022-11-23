84e26eba3fe6345c56370ca67706ab894dc0b96e
Jay T <jaytula@gmail.com>
2022-11-23 08:36:35 -0800

    Finish Video

--- src/main.rs
+++ src/main.rs
@@ -604,6 +604,39 @@ fn main_lesson_43() {
   }).join().unwrap();
 }
 
-fn main() {
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::sync::{Arc, Mutex};
 
+fn main() {
+  pub struct Bank {
+    balance: f32
+  }
+  fn withdraw(the_bank: &Arc<Mutex<Bank>>, amt: f32){
+    let mut bank_ref = the_bank.lock().unwrap();
+    if bank_ref.balance < 5.00 {
+      println!("Current balance : {} Withdrawal a smaller abount", bank_ref.balance);
+    } else {
+      bank_ref.balance -= amt;
+      println!("Customer withdrew {} Current Balance {}", amt, bank_ref.balance);
+    }
+  }
+
+  fn customer(the_bank: Arc<Mutex<Bank>>) {
+    withdraw(&the_bank, 5.00);
+  }
+
+  let bank: Arc<Mutex<Bank>> = Arc::new(Mutex::new(Bank{balance: 20.00}));
+  let handles = (0..10).map(|_| {
+    let bank_ref = bank.clone();
+    thread::spawn(|| {
+      customer(bank_ref);
+    })
+  });
+
+  for handle in handles {
+    handle.join().unwrap();
+  }
+
+  println!("Total {}", bank.lock().unwrap().balance);
 }
