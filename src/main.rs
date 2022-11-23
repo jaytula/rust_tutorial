db86fb42fcf12723bfa703203fb2dadb80aa83ff
Jay T <jaytula@gmail.com>
2022-11-23 08:16:55 -0800

    Video Progress 2:23:18

--- src/main.rs
+++ src/main.rs
@@ -583,4 +583,14 @@ fn main_lesson_42() {
 }
 
 fn main() {
+  pub struct Bank {
+    balance: f32
+  }
+  fn withdraw(the_bank: &mut Bank, amt: f32) {
+    the_bank.balance -= amt;
+  }
+
+  let mut bank = Bank{balance: 100.0};
+  withdraw(&mut bank, 5.00);
+  println!("Balance : {}", bank.balance);
 }
